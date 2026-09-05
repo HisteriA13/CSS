c46240707918095e3e6eddc302c8f746947b5e6e
Hilda Hadzic <13hilda@gmail.com>
2026-09-05 12:14:48 +0200

    Add arrow function

--- customScrollEffects/script.js
+++ customScrollEffects/script.js
@@ -1,11 +1,23 @@
 // background
 const bgImg = document.querySelector(".bg-img");
+const arrow = document.getElementById("arrow");
 
 window.addEventListener("scroll", () => {
-  handleImage();
+  handleBackground();
+  handleArrow();
 });
 
-function handleImage() {
+function handleBackground() {
   bgImg.style.opacity = 1 - window.pageYOffset / 900; // number 900 handles whiteness
   bgImg.style.backgroundSize = 100 + window.pageYOffset / 15 + "%"; // number 15 handles zooming
+}
+
+function handleArrow() {
+  if (window.scrollY < 5) {
+    arrow.classList.add("fade-in");
+    arrow.classList.remove("fade-out");
+  } else {
+    arrow.classList.remove("fade-in");
+    arrow.classList.add("fade-out");
+  }
 }
